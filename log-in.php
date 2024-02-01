a5bc483b7b97e3824812a09e7abeafa6fa357d77
deasejdiu <ds64830@ubt-uni.net>
2024-02-01 13:04:41 +0100

    .

--- log-in.php
+++ log-in.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    
     <form id="registrationForm" onsubmit="validateForm(); return false;">
         
         <label for="username">Username:</label>
@@ -15,6 +16,19 @@
 
         <label for="password">Password:</label>
         <input type="password" id="password" name="password" required>
+        
+        <?php 
+                        if(isset($_GET['loginError']) && $_GET['loginError']){
+                            ?>
+                            
+                            <div class="forminputerror" id="usernameError">
+                                <?php echo $_GET['loginError']; ?>
+
+                            </div>
+                            
+                            <?php
+                        }
+                    ?>
 
         <button type="submit" onclick="validateForm()">Submit</button>
     </form>
